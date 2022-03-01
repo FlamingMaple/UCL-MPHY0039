3735a0e1bfd226edc4cfd51c4a005c1a08587546
Oskar <96746788+FlamingMaple@users.noreply.github.com>
2022-03-01 12:43:03 +0000

    Fixed

--- src/embedded/embedded.ino
+++ src/embedded/embedded.ino
@@ -1,32 +1,11 @@
 #include <compat/deprecated.h>
 #include <FlexiTimer2.h>
-#include <Servo.h>
-// http://www.arduino.cc/playground/Main/FlexiTimer2
 
 // All definitions
 #define HEADERLEN 4
 #define PACKETLEN (HEADERLEN + 3)
 #define SAMPFREQ 256 // ADC sampling rate 256 Hz
 #define TIMER2VAL (1024/(SAMPFREQ)) // Set 256Hz sampling frequency
-
-// Define servo objects for fingers
-Servo s_1; // Finger 1
-Servo s_2; // Finger 2
-Servo s_3; // Finger 3
-Servo s_4; // Finger 4
-Servo s_T; // Thumb
-
-// What GPIO is each fingers' servo connected to?
-#define FINGER1 3
-#define FINGER2 5
-#define FINGER3 6
-#define FINGER4 10
-#define THUMB 11
-
-int M[5]; // Motor command control integer array which is read from serial
-
-// Used for storing servo values during serial read
-unsigned char a1, a2; 
 
 // Global constants and variables
 // These are volatile as they are handled in the ISR
@@ -54,21 +33,6 @@ void setup() {
  // Whenever interrupt occures, the current read packet is sent to the PC
  FlexiTimer2::set(TIMER2VAL, Timer2_Overflow_ISR);
  FlexiTimer2::start();
-
- // Attaches the servo on the GPIO (e.g. 3 default for finger 1) to the desired servo object
- s_1.attach(FINGER1);  
- s_2.attach(FINGER2); 
- s_3.attach(FINGER3);
- s_4.attach(FINGER4);
- s_T.attach(THUMB);
- delay (500);
-
- // Writes default position values to each servo
- s_1.write(90);
- s_2.write(90);
- s_3.write(90);
- s_4.write(90);
- s_T.write(120);
 
  interrupts(); // Enable all interrupts after initialization has been completed
 }
@@ -106,22 +70,4 @@ void Timer2_Overflow_ISR() {
 }
 
 void loop() {
-  // The loop() handles reading the desired servo values over serial and applying them
-   if (Serial.available() > 9) {
-      for (int i = 0; i<5; i++) {
-        // Read the servo positions over serial and parse them into the correct format
-        a1 = Serial.read();
-        M[i] = (int) a1;
-        M[i] = M[i]*256;
-        a2 = Serial.read();
-        M[i] |= (int) a2;
-      }
-
-      s_1.write(M[0]);
-      s_2.write(M[1]);
-      s_3.write(M[2]);
-      s_4.write(M[3]);
-      s_T.write(M[4]);
-
-   }
 }
